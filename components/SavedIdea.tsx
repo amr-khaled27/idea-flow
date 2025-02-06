e57e688ad84c6be44a660b944b0eb6a97139236b
amr-khaled27 <3moorkh2005@gmail.com>
2025-02-06 19:26:07 +0200

    Add loading state to delete button in SavedIdea component

--- components/SavedIdea.tsx
+++ components/SavedIdea.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { Idea } from "@/app/gemini";
 import { handleDeleteIdea, fetchIdeas } from "@/app/utils/firestore";
 import { User } from "firebase/auth";
 import { TrashIcon, PencilIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 type SavedIdeaProps = {
   idea: Idea;
@@ -11,6 +14,7 @@ type SavedIdeaProps = {
 };
 
 const SavedIdea = ({ idea, user, setIdeas }: SavedIdeaProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
   return (
     <div className="mb-4 w-full p-4 flex justify-between border rounded-lg shadow-sm">
       <div>
@@ -20,13 +24,19 @@ const SavedIdea = ({ idea, user, setIdeas }: SavedIdeaProps) => {
       <div className="flex flex-col">
         <Button
           onClick={async () => {
+            setLoading(true);
             await handleDeleteIdea(idea, user);
             const updatedIdeas = await fetchIdeas(user);
+            setLoading(false);
             setIdeas(updatedIdeas);
           }}
           variant="ghost"
         >
-          <TrashIcon className="h-5 w-5 text-red-500" />
+          {loading ? (
+            <BeatLoader size={4} color="#cacaca" />
+          ) : (
+            <TrashIcon className="h-5 w-5 text-red-500" />
+          )}
         </Button>
         <Button variant="ghost">
           <PencilIcon className="h-5 w-5 text-blue-500" />
